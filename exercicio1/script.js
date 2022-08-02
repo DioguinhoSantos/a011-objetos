7cc13a520c74a546ff0002fae6065931af24a4f7
DioguinhoSantos <diiogosantos@live.com>
2022-08-02 18:53:13 -0300

    Barbosa-A-Diogo-Santos

---
+++ exercicio1/script.js
@@ -0,0 +1,22 @@
+let sacolao = [];
+
+let cenoura = {
+    nome: `cenoura`,
+    preco: 6,
+    disponibilidade: true
+}
+let graoDeBico = {
+    nome: `grão de bico`,
+    preco: 4,
+    disponibilidade: true
+}
+let leite = {
+    nome: `Leite`,
+    preco: 7,
+    disponibilidade: false
+}
+
+sacolao.push(cenoura, graoDeBico, leite);
+
+console.log(`Sacolão com lista: `, sacolao);
+
